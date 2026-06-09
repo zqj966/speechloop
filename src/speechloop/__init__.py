11910b9ad939af119b51aa5fe42ecba6c36b9820
Lost <208735675+zqj966@users.noreply.github.com>
2026-06-09 16:07:31 +0800

    chore: bump version to 0.1.0

--- src/speechloop/__init__.py
+++ src/speechloop/__init__.py
@@ -6,7 +6,7 @@
 """
 from __future__ import annotations
 
-__version__ = "0.3.0"
+__version__ = "0.1.0"
 
 from .suite import Suite, load_suite
 from .runner import run_suite, CaseResult, SuiteResult
